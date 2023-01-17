6f7c4eb739e11c95ee62958d816e9bc58c9dd7d2
summmermin <110873921+summmermin@users.noreply.github.com>
2023-01-17 15:48:56 +0900

    enum 사용

---
+++ enum.ts
@@ -0,0 +1,10 @@
+enum Summer {
+    // 열거형 데이터 타입
+    // 멤버라 불리는 명명된 값의 집합을 이루는 자료형/ 값을 할당할수도 있음
+    name = 'name',
+    num = 100,
+    pet = 'pet',
+    job = 'job'
+}
+
+const x: string = Summer.name
